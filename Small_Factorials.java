e6dc3984d88f4246c58bbf7f5148e6149b9efac5
Lakshmi Prasanna Lahari Vommi <83864997+laharivommi3@users.noreply.github.com>
2022-09-10 21:59:49 +0530

    import java.util.Scanner; class Main { 	public static void main (String args[]) 	{ 		Scanner sc=new Scanner(System.in); 		 int t=sc.nextInt(); 	    while(t>0) 	    { 	        int n=sc.nextInt(); 	        int fact=1; 		    for(int i=1;i<=n;i++) 		    { 		         fact=fact*i; 		    } 		    System.out.println(fact); 		    t--; 		} 	} }

---
+++ Small_Factorials.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Main
+{
+	public static void main (String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		 int t=sc.nextInt();
+	    while(t>0)
+	    {
+	        int n=sc.nextInt();
+	        int fact=1;
+		    for(int i=1;i<=n;i++)
+		    {
+		         fact=fact*i;
+		    }
+		    System.out.println(fact);
+		    t--;
+		}
+	}
+}
