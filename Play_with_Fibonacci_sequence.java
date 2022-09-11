4c1854284a80b61b8d1b848624f12b6a2e4049a8
Lakshmi Prasanna Lahari Vommi <83864997+laharivommi3@users.noreply.github.com>
2022-09-11 16:14:32 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int x=0,y=1;         System.out.print(x+" "+y+" ");         for(int i=2;i<n;i++)         {         int sum=x+y;         x=y;         y=sum;         System.out.print(sum+" ");         }     } }

---
+++ Play_with_Fibonacci_sequence.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int x=0,y=1;
+        System.out.print(x+" "+y+" ");
+        for(int i=2;i<n;i++)
+        {
+        int sum=x+y;
+        x=y;
+        y=sum;
+        System.out.print(sum+" ");
+        }
+    }
+}
