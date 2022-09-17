ee33adf8384eb02a58acffd2fdf4da4f92b46e15
Lakshmi Prasanna Lahari Vommi <83864997+laharivommi3@users.noreply.github.com>
2022-09-17 15:39:34 +0530

    import java.util.Scanner; class Main {      public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int x[],i,n;         float sum=0;         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)         x[i]=sc.nextInt();         for(i=0;i<n;i++)         {           sum=sum+x[i];         }         System.out.format("%.2f",sum/n);     } }

---
+++ average_of_the_array.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Main
+{
+     public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int x[],i,n;
+        float sum=0;
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+        x[i]=sc.nextInt();
+        for(i=0;i<n;i++)
+        {
+          sum=sum+x[i];
+        }
+        System.out.format("%.2f",sum/n);
+    }
+}
