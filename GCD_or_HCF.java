9ec62f9da64c5a77a01ebb4d09d917e10c3e6662
Lakshmi Prasanna Lahari Vommi <83864997+laharivommi3@users.noreply.github.com>
2022-09-23 20:47:55 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int m,n,min=0,gcd=0;         m=sc.nextInt();         n=sc.nextInt();         if(m<n)         min=m;         else         min=n;         for(int i=min;i>0;i--)         {             if(m%i==0 && n%i==0)             {                 gcd=i;                 break;             }         }        System.out.println(gcd);        sc.close();     } }

--- GCD_or_HCF.java
+++ GCD_or_HCF.java
@@ -4,21 +4,22 @@ class Main
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int a,b,min,gcd=0;
-        a=sc.nextInt();
-        b=sc.nextInt();
-        if(a>b)
-        min=a;
-        else 
-        min=b;
+        int m,n,min=0,gcd=0;
+        m=sc.nextInt();
+        n=sc.nextInt();
+        if(m<n)
+        min=m;
+        else
+        min=n;
         for(int i=min;i>0;i--)
         {
-            if(a%i==0 && b%i==0)
+            if(m%i==0 && n%i==0)
             {
                 gcd=i;
                 break;
             }
         }
-        System.out.println(gcd);
+       System.out.println(gcd); 
+       sc.close();
     }
 }
