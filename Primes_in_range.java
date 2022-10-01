0841e530f5f8be051fe925c95b0cf83c03241060
Lakshmi Prasanna Lahari Vommi <83864997+laharivommi3@users.noreply.github.com>
2022-10-01 16:48:10 +0530

    import java.util.Scanner; class Main {     public static boolean isPrime(int n)     {         if(n<2)         return false;         int c=0;         for(int i=2;i<=(int)Math.sqrt(n);i++)         {             if(n%i==0)             c++;         }         if(c==0)         return true;         else         return false;     }     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int m,n,count=0;         m=sc.nextInt();         n=sc.nextInt();         for(int i=m;i<=n;i++)         {             if(isPrime(i))             count++;         }         System.out.println(count);     }
    }

--- Primes_in_range.java
+++ Primes_in_range.java
@@ -3,30 +3,31 @@ class Main
 {
     public static boolean isPrime(int n)
     {
-        int count=0;
+        if(n<2)
+        return false;
+        int c=0;
         for(int i=2;i<=(int)Math.sqrt(n);i++)
         {
             if(n%i==0)
-            count++;
+            c++;
         }
-        if(count==0)
+        if(c==0)
         return true;
-        else 
+        else
         return false;
     }
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int a,b,c=0;
-        a=sc.nextInt();
-        b=sc.nextInt();
-        if(a==1)
-        a=2;
-        for(int i=a;i<=b;i++)
+        int m,n,count=0;
+        m=sc.nextInt();
+        n=sc.nextInt();
+        for(int i=m;i<=n;i++)
         {
             if(isPrime(i))
-            c++;
+            count++;
         }
-        System.out.println(c);
+        System.out.println(count);
     }
+    
 }
