b45fd0ae323b90f3b3efb134b6396fc431b48fef
Lakshmi Prasanna Lahari Vommi <83864997+laharivommi3@users.noreply.github.com>
2022-10-03 11:32:53 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int x[],n,i,sum1=0,sum2=0,res;         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)         x[i]=sc.nextInt();         for(i=0;i<n;i++)         {             if(i%2!=0)             sum1=sum1+x[i];         }         for(i=0;i<n;i++)         {             if(i%2==0)             sum2=sum2+x[i];         }        res=Math.abs(sum1-sum2);        System.out.println(res);     } }

---
+++ Absolute_difference_b/w_sum_of_even_and_sum_of_odd_indexed_elements.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int x[],n,i,sum1=0,sum2=0,res;
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+        x[i]=sc.nextInt();
+        for(i=0;i<n;i++)
+        {
+            if(i%2!=0)
+            sum1=sum1+x[i];
+        }
+        for(i=0;i<n;i++)
+        {
+            if(i%2==0)
+            sum2=sum2+x[i];
+        }
+       res=Math.abs(sum1-sum2);
+       System.out.println(res);
+    }
+}
