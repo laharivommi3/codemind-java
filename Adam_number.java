e9f02d208352f946e646185f994d8e0fbb5d1e25
Lakshmi Prasanna Lahari Vommi <83864997+laharivommi3@users.noreply.github.com>
2022-10-03 10:32:21 +0530

    import java.util.Scanner; class Main {     public static int isReverse(int n)     {         int r,sum=0;         while(n>0)         {             r=n%10;             sum=sum*10+r;             n=n/10;         }         return sum;     }     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,sq1,sq2,res1,res2=0;         n=sc.nextInt();         sq1=n*n;         res1=isReverse(n);         sq2=res1*res1;         res2=isReverse(sq2);         if(sq1==res2)         System.out.println("True");         else         System.out.println("False");     } }

--- Adam_number.java
+++ Adam_number.java
@@ -15,19 +15,15 @@ class Main
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int n,sq1,sq2,res1,res2;
+        int n,sq1,sq2,res1,res2=0;
         n=sc.nextInt();
-        sq1=n*n; //144
-        res1=isReverse(n); //21
-        sq2=res1*res1; //441
-        res2=isReverse(sq2); //144
+        sq1=n*n;
+        res1=isReverse(n);
+        sq2=res1*res1;
+        res2=isReverse(sq2);
         if(sq1==res2)
-        {
         System.out.println("True");
-        }
         else
-        {
         System.out.println("False");
-        }
     }
 }
