1b35b0bd578d4f28256c4b1d404297f38d2acd92
Lakshmi Prasanna Lahari Vommi <83864997+laharivommi3@users.noreply.github.com>
2022-10-09 15:54:35 +0530

    import java.util.Scanner; class Main {     public static boolean isPrime(int n)     {         int count=0;         if(n==1)         return false;         for(int i=2;i<=(int)Math.sqrt(n);i++)         {             if(n%i==0)             count++;         }         if(count==0)         return true;         else         return false;     }     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int x[],n,i,c=0;         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)         x[i]=sc.nextInt();         for(i=0;i<n;i++)         {             if(isPrime(x[i]))             c++;         }         System.out.println(c);     } }

--- Primes_in_the_array.java
+++ Primes_in_the_array.java
@@ -1,25 +1,25 @@
 import java.util.Scanner;
 class Main
 {
-        public static boolean isPrime(int n)
+    public static boolean isPrime(int n)
+    {
+        int count=0;
+        if(n==1)
+        return false;
+        for(int i=2;i<=(int)Math.sqrt(n);i++)
         {
-            int count=0;
-            if(n==1)
-            return false;
-            for(int i=2;i<=(int)Math.sqrt(n);i++)
-            {
-                if(n%i==0)
-                count++;
-            }
-            if(count==0)
-            return true;
-            else
-            return false;
+            if(n%i==0)
+            count++;
         }
-        public static void main(String args[])
-        {
+        if(count==0)
+        return true;
+        else
+        return false;
+    }
+    public static void main(String args[])
+    {
         Scanner sc=new Scanner(System.in);
-        int x[],i,n,count=0;
+        int x[],n,i,c=0;
         n=sc.nextInt();
         x=new int[n];
         for(i=0;i<n;i++)
@@ -27,8 +27,8 @@ class Main
         for(i=0;i<n;i++)
         {
             if(isPrime(x[i]))
-            count++;
+            c++;
         }
-        System.out.println(count);
+        System.out.println(c);
     }
 }
