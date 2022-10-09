c38fd832704a91b2afe71563c34483265ee45d8d
Lakshmi Prasanna Lahari Vommi <83864997+laharivommi3@users.noreply.github.com>
2022-10-09 19:34:56 +0530

    import java.util.Scanner; class Main {     public static boolean isPrime(int n)     {         int count=0;         if(n<1)         return false;         for(int i=2;i<=(int)Math.sqrt(n);i++)         {             if(n%i==0)             count++;         }         if(count==0)         return true;         else         return false;     }     public static boolean isPalindrome(int n)     {         int sum=0,r,temp=n;         while(n>0)         {             r=n%10;             sum=sum*10+r;             n=n/10;         }         if(temp==sum)         return true;         else         return false;     }     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,i;         n=sc.nextInt();         for(i=n+1;;i++)         {             if(isPrime(i) && isPalindrome(i))             {                 break;             }         }         System.out.println(i);     } }

--- Program_to_find_out_the_next_prime_palindrome_number.java
+++ Program_to_find_out_the_next_prime_palindrome_number.java
@@ -4,10 +4,12 @@ class Main
     public static boolean isPrime(int n)
     {
         int count=0;
+        if(n<1)
+        return false;
         for(int i=2;i<=(int)Math.sqrt(n);i++)
         {
             if(n%i==0)
-              count++;
+            count++;
         }
         if(count==0)
         return true;
@@ -16,31 +18,30 @@ class Main
     }
     public static boolean isPalindrome(int n)
     {
-         int temp=n;
-         int sum=0,r;
-         while(n>0)
-         {
-             r=n%10;
-             sum=sum*10+r;
-             n=n/10;
-         }
-         if(temp==sum)
-         return true;
-         else
-         return false;
+        int sum=0,r,temp=n;
+        while(n>0)
+        {
+            r=n%10;
+            sum=sum*10+r;
+            n=n/10;
+        }
+        if(temp==sum)
+        return true;
+        else
+        return false;
     }
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
         int n,i;
         n=sc.nextInt();
-        for(i=n+1; ;i++)
+        for(i=n+1;;i++)
         {
-          if(isPrime(i) && isPalindrome(i))
-          {
-          System.out.println(i);
-          break;
-          }
+            if(isPrime(i) && isPalindrome(i))
+            {
+                break;
+            }
         }
+        System.out.println(i);
     }
 }
