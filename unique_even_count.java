8421084a23ea24e988f9ae8159746137a0b52af0
Lakshmi Prasanna Lahari Vommi <83864997+laharivommi3@users.noreply.github.com>
2022-10-10 23:30:36 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[],i,j,count=0;         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)         x[i]=sc.nextInt();         for(i=0;i<n;i++)         {             for(j=0;j<i;j++)             {                 if(x[i]==x[j])                 {                     break;                 }             }             if(i==j && x[i]%2==0)             {                 count++;             }         }         System.out.println(count);     } }

---
+++ unique_even_count.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,x[],i,j,count=0;
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+        x[i]=sc.nextInt();
+        for(i=0;i<n;i++)
+        {
+            for(j=0;j<i;j++)
+            {
+                if(x[i]==x[j])
+                {
+                    break;
+                }
+            }
+            if(i==j && x[i]%2==0)
+            {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
