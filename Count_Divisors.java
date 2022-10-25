57a94c1663487ba94b6a5b7338abd87b0b67a853
Lakshmi Prasanna Lahari Vommi <83864997+laharivommi3@users.noreply.github.com>
2022-10-25 12:31:08 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int l,r,k,c=0;         l=sc.nextInt();         r=sc.nextInt();         k=sc.nextInt();         for(int i=l;i<=r;i++)         {             if(i%k==0)             {                 c++;             }         }         System.out.println(c);     } }

---
+++ Count_Divisors.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int l,r,k,c=0;
+        l=sc.nextInt();
+        r=sc.nextInt();
+        k=sc.nextInt();
+        for(int i=l;i<=r;i++)
+        {
+            if(i%k==0)
+            {
+                c++;
+            }
+        }
+        System.out.println(c);
+    }
+}
