2932715c54e88e52d381f2406c51183b1d274dd1
Lakshmi Prasanna Lahari Vommi <83864997+laharivommi3@users.noreply.github.com>
2022-11-04 22:00:32 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[],i,a,b,min,c=0;         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)         x[i]=sc.nextInt();         min=0;         a=sc.nextInt();         b=sc.nextInt();         for(i=0;i<n;i++)         {             if(x[i]>=a && x[i]<=b)             {                 if(x[i]>min)                 min=x[i];                 c++;             }         }         if(c<=0)         System.out.println("-1");         else         System.out.println(min);     } }

--- maximum_of_numbers_between_A_and_B.java
+++ maximum_of_numbers_between_A_and_B.java
@@ -4,26 +4,26 @@ class Main
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int x[],n,i,a,b,max,c=0;
+        int n,x[],i,a,b,min,c=0;
         n=sc.nextInt();
         x=new int[n];
         for(i=0;i<n;i++)
         x[i]=sc.nextInt();
-        max=x[0];
+        min=0;
         a=sc.nextInt();
         b=sc.nextInt();
         for(i=0;i<n;i++)
         {
             if(x[i]>=a && x[i]<=b)
             {
-                if(x[i]>max)
-                max=x[i];
+                if(x[i]>min)
+                min=x[i];
                 c++;
             }
         }
         if(c<=0)
         System.out.println("-1");
         else
-        System.out.println(max);
+        System.out.println(min);
     }
 }
