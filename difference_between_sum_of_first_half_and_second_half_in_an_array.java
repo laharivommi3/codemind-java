59ef8e0f3f8a6de5454e31e313d2d03174a7f204
Lakshmi Prasanna Lahari Vommi <83864997+laharivommi3@users.noreply.github.com>
2022-11-04 22:14:47 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[],i,sum1=0,sum2=0;         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)         x[i]=sc.nextInt();         for(i=0;i<n/2;i++)         {             sum1=sum1+x[i];         }         for(i=n/2;i<n;i++)         {             sum2=sum2+x[i];         }         System.out.println(sum2-sum1);     } }

---
+++ difference_between_sum_of_first_half_and_second_half_in_an_array.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,x[],i,sum1=0,sum2=0;
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+        x[i]=sc.nextInt();
+        for(i=0;i<n/2;i++)
+        {
+            sum1=sum1+x[i];
+        }
+        for(i=n/2;i<n;i++)
+        {
+            sum2=sum2+x[i];
+        }
+        System.out.println(sum2-sum1);
+    }
+}
