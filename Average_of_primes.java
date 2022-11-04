ef503b4c068f2a709ac93cb6b3c7b5e1674d3df1
Lakshmi Prasanna Lahari Vommi <83864997+laharivommi3@users.noreply.github.com>
2022-11-04 22:27:50 +0530

    import java.util.Scanner; class Main {     public static boolean isPrime(int n)     {         int c=0;         if(n==1)         return false;         for(int i=2;i<=(int)Math.sqrt(n);i++)         {             if(n%i==0)             {                 c++;             }         }         if(c==0)         return true;         else         return false;     }     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[],i;         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)         x[i]=sc.nextInt();         int count=0;         float sum=0,avg=0;         for(i=0;i<n;i++)         {             if(isPrime(x[i]))             {             count++;             sum=sum+x[i];             }         }         avg=sum/count;         System.out.format("%.2f",avg);     } }

--- Average_of_primes.java
+++ Average_of_primes.java
@@ -1,30 +1,33 @@
 import java.util.Scanner;
 class Main
 {
-        public static boolean isPrime(int n)
+    public static boolean isPrime(int n)
+    {
+        int c=0;
+        if(n==1)
+        return false;
+        for(int i=2;i<=(int)Math.sqrt(n);i++)
         {
-            int count=0;
-            if(n==1)
-            return false;
-            for(int i=2;i<=(int)Math.sqrt(n);i++)
+            if(n%i==0)
             {
-                if(n%i==0)
-                count++;
+                c++;
             }
-            if(count==0)
-            return true;
-            else
-            return false;
         }
-        public static void main(String args[])
-        {
+        if(c==0)
+        return true;
+        else
+        return false;
+    }
+    public static void main(String args[])
+    {
         Scanner sc=new Scanner(System.in);
-        int x[],i,n,count=0;
-        float sum=0;
+        int n,x[],i;
         n=sc.nextInt();
         x=new int[n];
         for(i=0;i<n;i++)
         x[i]=sc.nextInt();
+        int count=0;
+        float sum=0,avg=0;
         for(i=0;i<n;i++)
         {
             if(isPrime(x[i]))
@@ -33,6 +36,7 @@ class Main
             sum=sum+x[i];
             }
         }
-        System.out.format("%.2f",sum/count);
+        avg=sum/count;
+        System.out.format("%.2f",avg);
     }
 }
