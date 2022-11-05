d59b2d47a5ed6b2af3495c0bce3a4ff0f82f5f1f
Lakshmi Prasanna Lahari Vommi <83864997+laharivommi3@users.noreply.github.com>
2022-11-05 21:22:39 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[],i,sum=0,even=0;         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)         x[i]=sc.nextInt();         for(i=0;i<n;i++)         {             if(x[0]%2==0)             {                 break;             }             else             {             sum=sum+x[i];             if(x[i]%2==0)             {                even=x[i];                break;             }             }         }         System.out.println(sum-even);     } }

---
+++ sum_of_array__elements_upto__first_even_number.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,x[],i,sum=0,even=0;
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+        x[i]=sc.nextInt();
+        for(i=0;i<n;i++)
+        {
+            if(x[0]%2==0)
+            {
+                break;
+            }
+            else
+            {
+            sum=sum+x[i];
+            if(x[i]%2==0)
+            {
+               even=x[i];
+               break;
+            }
+            }
+        }
+        System.out.println(sum-even);
+    }
+}
