0c38e84f5562326a943b1ea7443eb6b187461524
Lakshmi Prasanna Lahari Vommi <83864997+laharivommi3@users.noreply.github.com>
2022-11-09 13:58:28 +0530

    import java.util.Scanner; import java.lang.String; class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String s1=sc.nextLine();         int c=0;         for(int i=0;i<s1.length();i++)         {             char ch=s1.charAt(i);             if(ch==' ')             {                 continue;             }             else             {                 c++;             }         }         System.out.println(c);     } }

---
+++ without_spaces_number_of_characters.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.lang.String;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s1=sc.nextLine();
+        int c=0;
+        for(int i=0;i<s1.length();i++)
+        {
+            char ch=s1.charAt(i);
+            if(ch==' ')
+            {
+                continue;
+            }
+            else
+            {
+                c++;
+            }
+        }
+        System.out.println(c);
+    }
+}
